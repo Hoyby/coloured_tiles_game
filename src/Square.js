9418311e2962be33385b153f2fd129caa901ce56
Hoeyby <alex.hoeyby@gmail.com>
2020-12-04 18:06:43 +0100

    log tiles to change

--- src/Square.js
+++ src/Square.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Square(square, changeColor) {
 
     function handleSquareClick(){
-        console.log("hei")
+        console.log("change colour of tile nr.", square.id)
     }
 
     return <button class="square" id={square.id} style={{backgroundColor: square.colour}} onClick={handleSquareClick} />
